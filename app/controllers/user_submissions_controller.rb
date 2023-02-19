2327a0b96ca108f7595175241719d22078124529
Matt Quinn <mquinn2489@gmail.com>
2023-02-18 18:07:30 -0700

    form helpers adding for text and db create

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -1,11 +1,12 @@
 class UserSubmissionsController < ApplicationController
   def create
-
+    UserSubmission.create!(user_submission_params)
+    redirect_to root_path
   end
 
 
   def user_submission_params
-    params.require(:user_submission).permit(:first_name, :last_name, :email, :website) 
+    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :job_role, :text)
   end
 
 
